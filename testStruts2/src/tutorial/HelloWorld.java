0c2c6978e307650b071da01eddc7d6f41711f050
kuangjg <1358866430@qq.com>
2016-09-06 00:56:24 +0800

    struts2 sample

---
+++ testStruts2/src/tutorial/HelloWorld.java
@@ -0,0 +1,41 @@
+package tutorial;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorld extends ActionSupport {
+	public final static String MESSAGE = "Struts2 is up and running ...";  
+    
+    private String message;  
+  
+  
+    /** 
+     * @return the message 
+     */  
+    public String getMessage()  
+    {  
+        return message;  
+    }  
+  
+  
+    /** 
+     * @param message the message to set 
+     */  
+    public void setMessage(String message)  
+    {  
+        this.message = message;  
+    }  
+  
+  
+    public String execute() throws Exception  
+    {  
+        setMessage(MESSAGE);  
+        return SUCCESS;  
+    }  
+    
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7641460633307331299L;
+
+}
